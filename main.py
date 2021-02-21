fffb4120bea2bcba5ca4ee8cd15c0f936fe3a529
Ali BENMERAD <60623854+abenmerad@users.noreply.github.com>
2021-02-21 11:57:07 +0100

    Niveau 1
    Première étape de la mise en place du jeu du petit prince dans le cadre du mooc apprendre python sur la plateforme funmooc

---
+++ main.py
@@ -0,0 +1,11 @@
+from CONFIGS import *
+# from FONCTIONS import *
+
+
+def lire_matrice(fichier):
+    matrice = []
+    for ligne in open(fichier):
+        for lettre in ligne:
+            
+    return matrice
+print(lire_matrice(fichier_plan))
---
+++ venv/FONCTIONS.py
@@ -0,0 +1,1 @@
+def lire_matrice(fichier):
